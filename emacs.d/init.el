a4fc255857fb0fddbc082d81a595d3774f5b3189
Takuto Ikuta <atetubou@gmail.com>
2016-09-17 09:39:25 +0900

    add nginx mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -80,3 +80,10 @@
 
 (let ((envs '("PATH" "GOPATH")))
   (exec-path-from-shell-copy-envs envs))
+
+
+;;(add-to-list 'auto-mode-alist '("nginx\\.conf$" . nginx-mode))
+(add-hook 'conf-mode-hook
+          (lambda ()
+            (when (string-match "nginx" (buffer-file-name))
+              (nginx-mode))))
