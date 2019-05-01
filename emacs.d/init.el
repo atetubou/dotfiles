3a2873dd7968c955ba815e46cfab1b9545f1d9f7
tikuta <tikuta@linux>
2019-05-01 16:29:20 +0900

    use straight.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,32 +1,33 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(helm-company company-lsp company flycheck helm rainbow-delimiters use-package go-mode lsp-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;;; global configurations
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (keyboard-translate ?\C-h ?\C-?)
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backup")))
 (setq default-tab-width 4)
 (show-paren-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+
+;;; https://nukosuke.hatenablog.jp/entry/straight-el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+
 
 ;;; package specific configurations
 (use-package company
@@ -58,11 +59,6 @@
   (add-hook 'before-save-hook 'lsp-format-buffer))
 
 (use-package helm
-  :defer t
-  :config
-  (helm-autoresize-mode t))
-
-(use-package helm-config
   ;;; Should not have ":defer t".
   :bind
   (
@@ -72,10 +68,11 @@
    ("M-y" . 'helm-show-kill-ring)
    )
   :config
+  (helm-autoresize-mode t)
   (helm-mode 1)
 )
 
-(use-package rainbow-delimiters-mode
+(use-package rainbow-delimiters
   :defer t
   :init
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
