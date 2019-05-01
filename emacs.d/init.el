344709c5a4fda6586518819173286cb1b1d8801e
tikuta <tikuta@localhost>
2019-05-01 15:47:15 +0900

    delay

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -34,6 +34,7 @@
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (global-company-mode)
+  (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 2)
   (setq company-selection-wrap-around t)
   (setq completion-ignore-case t)
